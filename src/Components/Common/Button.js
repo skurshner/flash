be4107bb67795ab6fe47ede516c21d1fd47aa64c
Stephen Kurshner <skurshner@gmail.com>
2021-10-12 16:44:04 -0700

    updated DeckListItem component with text and 2 of 3 buttons

--- src/Components/Common/Button.js
+++ src/Components/Common/Button.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Button = ({ name, type, text, icon }) => {
   const getButtonStyle = name => {
-    if (name === "create") {
+    if (name === "create" || name === "view") {
       return "btn btn-secondary btn-lg";
+    }
+    if (name === "study") {
+      return "btn btn-primary btn-lg";
     }
   };
 
@@ -11,11 +14,17 @@ const Button = ({ name, type, text, icon }) => {
     if (icon === "add") {
       return "bi bi-plus-lg";
     }
+    if (icon === "view") {
+      return "bi bi-eye-fill";
+    }
+    if (icon === "study") {
+      return "bi bi-book-half";
+    }
   };
 
   return (
     <button type={type} className={getButtonStyle(name)}>
-      <i style={{ paddingRight: "8px" }} className={getIcon(icon)}></i>
+      <i style={{ paddingRight: "12px" }} className={getIcon(icon)}></i>
       {text}
     </button>
   );
--- src/Components/Home/DeckListItem.js
+++ src/Components/Home/DeckListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../Common/Button";
 
 const DeckListItem = () => {
   return (
@@ -12,7 +13,39 @@ const DeckListItem = () => {
             <p className="text-end"># cards</p>
           </div>
         </div>
-        <p>Deck List Item</p>
+        <div className="row">
+          <div className="col-auto">
+            <p className="text-body">
+              This is an example of the description for a deck. There could be
+              multiple sentences here.
+            </p>
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-auto">
+            <div className="row">
+              <div className="col-auto pr-2">
+                <Button
+                  type={"button"}
+                  name={"view"}
+                  text={"View"}
+                  icon={"view"}
+                />
+              </div>
+              <div className="col-auto p-0">
+                <Button
+                  type={"button"}
+                  name={"study"}
+                  text={"Study"}
+                  icon={"study"}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-auto">
+            <Button />
+          </div>
+        </div>
       </div>
     </div>
   );
