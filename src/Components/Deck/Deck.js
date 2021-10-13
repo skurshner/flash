a0bbc90b59207673b6e7a4f2e9fb76918d7117c6
Stephen Kurshner <skurshner@gmail.com>
2021-10-13 09:00:45 -0700

    started getting deck info using useEffect

--- src/Components/Deck/Deck.js
+++ src/Components/Deck/Deck.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
+import { readDeck } from "../../utils/api";
 import Breadcrumbs from "../Common/Breadcrumbs";
 
 const Deck = () => {
   const deckId = useRouteMatch().params.deckId;
+
+  const [deck, setDeck] = useState({});
+
+  useEffect(() => {
+    setDeck({});
+    async function getDeck() {
+      const deck = await readDeck(deckId);
+      setDeck(deck);
+    }
+
+    getDeck();
+  }, []);
+
   return (
     <div className="container">
       <Breadcrumbs />
       <p>deck {deckId}</p>
+      <p>{deck.name}</p>
     </div>
   );
 };
