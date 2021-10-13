f63543d073bca1df58930143e67badfbaa5ebdd1
Stephen Kurshner <skurshner@gmail.com>
2021-10-13 08:57:08 -0700

    added breadcrumb component to Deck component

--- src/Components/Common/Breadcrumbs.js
+++ src/Components/Common/Breadcrumbs.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Breadcrumbs = () => {
-  return <p>Breadcrumbs</p>;
+  return (
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <Link to="/">
+            <i class="bi bi-house-door-fill mr-2"></i>Home
+          </Link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+          Library
+        </li>
+      </ol>
+    </nav>
+  );
 };
 
 export default Breadcrumbs;
--- src/Components/Deck/Deck.js
+++ src/Components/Deck/Deck.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { useRouteMatch } from "react-router-dom";
+import Breadcrumbs from "../Common/Breadcrumbs";
 
 const Deck = () => {
   const deckId = useRouteMatch().params.deckId;
-  return <p>Deck {deckId}</p>;
+  return (
+    <div className="container">
+      <Breadcrumbs />
+      <p>deck {deckId}</p>
+    </div>
+  );
 };
 
 export default Deck;
