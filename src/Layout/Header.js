96bbff7abd5f9f8f9a5b068b8f5c227ebfd3e28b
Stephen Kurshner <skurshner@gmail.com>
2021-10-15 16:59:47 -0700

    added image to header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="jumbotron bg-dark">
-      <div className="container text-white">
+    <header className="jumbotron ">
+      <div className="container text-light">
         <h1 className="display-4">Flashcard-o-matic</h1>
         <p className="lead">Discover The Flashcard Difference.</p>
       </div>
